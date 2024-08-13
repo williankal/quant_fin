b5e02da11e16fbb94f263711b92980a150e3a806
williankal <kenzo.willian@hotmail.com>
2024-08-12 23:03:59 -0300

    fix: changed scripts to a different folder

---
+++ scripts/sp500_get_infos.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+sp500_raw = pd.read_csv('../infos_30/random_30_stocks.csv')
+sp500Random= sp500_raw.copy()
+
+base = pd.read_csv('../archive/sp500_companies.csv')
+
+sp500Random = sp500Random.merge(base, on='Symbol', how='left')
+sp500Random.to_csv('../infos_30/sp500_companies_selected.csv', index=False)
--- sp500_get_infos.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-sp500_raw = pd.read_csv('infos_30/random_30_stocks.csv')
-sp500Random= sp500_raw.copy()
-
-base = pd.read_csv('archive/sp500_companies.csv')
-
-sp500Random = sp500Random.merge(base, on='Symbol', how='left')
-sp500Random.to_csv('sp500_ciompanies_selected.csv', index=False)
